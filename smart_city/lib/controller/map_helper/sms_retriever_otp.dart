19f777775a622011114c4c0da9ef70126324937c
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-15 14:20:42 +0700

    feat: autofill otp from sms from user content API

---
+++ smart_city/lib/controller/map_helper/sms_retriever_otp.dart
@@ -0,0 +1,26 @@
+import 'package:pinput/pinput.dart';
+import 'package:smart_auth/smart_auth.dart';
+
+class SmsRetrieverOTP implements SmsRetriever{
+  const SmsRetrieverOTP(this.smartAuth);
+  final SmartAuth smartAuth;
+
+  @override
+  Future<void> dispose() {
+    return smartAuth.removeSmsListener();
+  }
+
+  @override
+  Future<String?> getSmsCode() async {
+    final res = await smartAuth.getSmsCode(
+      useUserConsentApi: true,
+    );
+    if (res.succeed && res.codeFound) {
+      return res.code!;
+    }
+    return null;
+  }
+
+  @override
+  bool get listenForMultipleSms => false;
+}
