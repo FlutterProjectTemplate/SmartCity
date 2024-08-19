a70c78d79256bb94eeeb67f27868d14aafa7bc0b
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:55:53 +0700

    refactor: add routes for screens

--- smart_city/lib/base/routes/routes.dart
+++ smart_city/lib/base/routes/routes.dart
@@ -1,7 +1,32 @@
-abstract class Routes{
+import 'package:go_router/go_router.dart';
+import 'package:smart_city/view/login/forgot_password/forgot_password_ui.dart';
+import 'package:smart_city/view/splash_screen.dart';
+import 'package:smart_city/view/login/login_ui.dart';
+import 'package:smart_city/view/map/map_ui.dart';
 
-}
-
-abstract class Paths{
-
-}
+final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        name: 'map',
+        path: '/map',
+        builder: (context, state) => const MapUi(),
+      ),
+      GoRoute(
+          path: '/login',
+          builder: (context, state) => const Login(),
+          routes: [
+            GoRoute(
+              path: 'forgot-password/:phone',
+              builder: (context, state) {
+                final phone = state.pathParameters['phone'];
+                return ForgotPassword(phoneNumber: phone??'');
+              },
+            )
+          ]
+      )
+]
+);
