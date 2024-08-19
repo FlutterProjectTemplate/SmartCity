2f71f1529b561acad1317e80f26d8253a89f0531
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:51:14 +0700

    refactor: create error screen when wrong routes

---
+++ smart_city/lib/base/routes/error_route_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ErrorRouteScreen extends StatelessWidget {
+  const ErrorRouteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: const Text("Error Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go("/"),
+          child: const Text("Go to welcome page"),
+        ),
+      ),
+    );
+  }
+}
