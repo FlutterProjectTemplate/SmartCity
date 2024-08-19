ad5deb6fc0597c8624673bb7766f3d74198ab868
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:53:35 +0700

    feat: create start and stop navigating feature

--- smart_city/lib/view/map/map_ui.dart
+++ smart_city/lib/view/map/map_ui.dart
@@ -4,6 +4,7 @@ import 'package:smart_city/base/widgets/button.dart';
 import 'package:smart_city/base/widgets/custom_container.dart';
 import 'package:smart_city/constant_value/const_colors.dart';
 import 'package:smart_city/constant_value/const_fonts.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class MapUi extends StatefulWidget {
   const MapUi({super.key});
@@ -14,44 +15,17 @@ class MapUi extends StatefulWidget {
 
 class _MapUiState extends State<MapUi> with SingleTickerProviderStateMixin {
   late GoogleMapController _controller;
-  late String _mapStyleString='';
-  late AnimationController _animationController;
-  late Animation<double> _animation;
-  bool hidden = true;
-  LatLng _currentUserLocation = const LatLng(20.980238, 105.844616);
+  late String _mapStyleString='';// map style
+  bool hidden = true;// show or hide the countdown timer
+  bool isCompleted = false; // check if the countdown timer is completed
+  final LatLng _currentUserLocation = const LatLng(20.980238, 105.844616);
 
   @override
   void initState() {
     DefaultAssetBundle.of(context).loadString('assets/dark_mode_style.json').then((string) {
       _mapStyleString = string;
     });
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this,
-    );
-
-    _animation = Tween<double>(begin: 3, end: 0).animate(_animationController)
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if(status == AnimationStatus.forward){
-          setState(() {
-            hidden = false;
-          });
-        }
-        if (status == AnimationStatus.completed) {
-          setState(() {
-            hidden = true;
-          });
-        }
-      });
     super.initState();
-  }
-  @override
-  void dispose() {
-    _animationController.dispose();
-      super.dispose();
   }
 
   @override
@@ -96,9 +70,25 @@ class _MapUiState extends State<MapUi> with SingleTickerProviderStateMixin {
           ),
           Align(
             alignment: Alignment.center,
-            child: Text(hidden?"":
-            _animation.value.toInt() == 0.0 ?"Start":_animation.value.toStringAsFixed(1),
-              style: ConstFonts().heading,),
+            child: hidden?const SizedBox():CircularCountDownTimer(
+              isReverse: true,
+              isReverseAnimation: false,
+              duration: 3,
+              width: width/3,
+              height: width/3,
+              fillColor: ConstColors.primaryColor,
+              ringColor: ConstColors.tertiaryContainerColor,
+              strokeWidth: 5.0,
+              strokeCap: StrokeCap.round,
+              autoStart: true,
+              textStyle: ConstFonts().heading,
+              onComplete: (){
+                setState(() {
+                  hidden = true;
+                  isCompleted = true;
+                });
+              },
+            )
           ),
           Positioned(
               bottom: 15,
@@ -120,19 +110,25 @@ class _MapUiState extends State<MapUi> with SingleTickerProviderStateMixin {
                         controlButton(icon: Icons.straight_rounded, onPressed: (){}),
                         controlButton(icon: Icons.turn_right_rounded, onPressed: (){}),
                         GestureDetector(
+                          onTap: (){
+                            if(isCompleted){
+                              _showDialog();
+                            }
+                          },
                           onLongPress:(){
-                            _animationController.forward(from: 0.0);
+                            setState(() {
+                              hidden = false;
+                            });
                           },
                           onLongPressEnd: (details){
-                            _animationController.reset();
                             setState(() {
                               hidden = true;
                             });
                           },
                           child:Button(
-                            width: 60, height: 60, color: ConstColors.primaryColor,
+                            width: 60, height: 60, color:isCompleted?ConstColors.errorColor: ConstColors.primaryColor,
                             isCircle: true,
-                            child: const Icon(Icons.play_arrow_rounded,color: ConstColors.tertiaryContainerColor,size:45,),
+                            child: Icon(isCompleted?Icons.pause:Icons.play_arrow_rounded,color: isCompleted?Colors.white:ConstColors.tertiaryContainerColor,size:45,),
                           ).getButton(),
                         )
                       ],
@@ -153,6 +149,54 @@ class _MapUiState extends State<MapUi> with SingleTickerProviderStateMixin {
     );
   }
 
+  void _showDialog() async{
+    showDialog(
+        context: context,
+        builder: (context)=>AlertDialog(
+          icon: const Icon(Icons.location_off_rounded,color: Colors.white,size: 45,),
+          title:  Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Stop tracking',style: ConstFonts().copyWithTitle(fontSize: 19),),
+              const SizedBox(height: 5,),
+              Text('Are you sure you want to stop?',style: ConstFonts().copyWithSubHeading(fontSize: 15)),
+              const SizedBox(height: 5,),
+            ],
+          ),
+          backgroundColor: ConstColors.surfaceColor,
+          actions: [
+            Button(
+                width: 105, height: 47,
+                color:ConstColors.errorContainerColor,
+                isCircle: false,
+                child:TextButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel",style: ConstFonts().copyWithTitle(fontSize: 16)),
+                )
+            ).getButton(),
+            const SizedBox(width: 20,),
+            Button(
+                width: 105, height: 47,
+                color:ConstColors.primaryColor,
+                isCircle: false,
+                child:TextButton(
+                  onPressed: (){
+                    setState(() {
+                      isCompleted = false;// reset the countdown timer
+                    });
+                    Navigator.pop(context);
+                  },
+                  child: Text("Resume",style: ConstFonts().copyWithTitle(fontSize: 16)),
+                )
+            ).getButton(),
+          ],
+        )
+    );
+  }
+
   Widget controlButton({required IconData icon,required Function() onPressed,Color? color}){
     return Button(
         width: 45, height: 45, color: Colors.white,
@@ -160,7 +204,7 @@ class _MapUiState extends State<MapUi> with SingleTickerProviderStateMixin {
         child:IconButton(
           onPressed: onPressed,
           icon: Center(
-              child: Icon(icon,color: color??ConstColors.secondaryContainerColor,size: 30,)),
+              child: Icon(icon,color: color??(isCompleted?ConstColors.surfaceColor:ConstColors.secondaryContainerColor),size: 30,)),
         )
     ).getButton();
   }
