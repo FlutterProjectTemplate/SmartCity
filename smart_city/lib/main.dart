373520e48346e8d588c3fbcb795b80f1ace063e4
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:53:56 +0700

    feat: add go router in main

--- smart_city/lib/main.dart
+++ smart_city/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:smart_city/base/routes/routes.dart';
+import 'package:smart_city/base/store/cached_storage.dart';
+import 'base/firebase_manager/firebase_manager.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  //await initialService();
+  //FirebaseMessaging.onBackgroundMessage((_)=>FirebaseManager.getInstance.firebaseMessagingBackgroundHandler(_));
   runApp(const MyApp());
+}
+
+Future<void> initialService()async {
+  await SharedPreferencesStorage().initSharedPreferences();
+  FirebaseManager.getInstance.initialFirebase();
 }
 
 class MyApp extends StatelessWidget {
@@ -9,8 +21,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Placeholder(),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
