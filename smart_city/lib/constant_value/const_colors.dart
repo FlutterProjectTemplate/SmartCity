56bcf6f973a1249fe6690b260bf6e68e6bb4e4ac
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:57:47 +0700

    refactor: add new color for container display information in map

--- smart_city/lib/constant_value/const_colors.dart
+++ smart_city/lib/constant_value/const_colors.dart
@@ -8,6 +8,7 @@ class ConstColors{
   static const Color secondaryContainerColor = Color(0xffD9D9D9);
   static const Color onSecondaryContainerColor = Colors.black54;
   static const Color tertiaryColor = Color(0xff8C8C8E);
+  static const Color tertiaryContainerColor = Color(0xff3C4C4D);
   static const Color errorColor = Color(0xffFF0000);
   static const Color errorContainerColor = Color(0xffFF6262);
   static const Color surfaceColor = Color(0xff24242B);
