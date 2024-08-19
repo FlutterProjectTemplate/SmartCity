cdff7e1959252f3f6f893a9cde2f7f8ec4eabe0d
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-19 16:59:22 +0700

    perf: improve logic of function checkConnectionInternet

--- smart_city/lib/base/services/base_request/internet_checker_handler.dart
+++ smart_city/lib/base/services/base_request/internet_checker_handler.dart
@@ -9,34 +9,35 @@ class InternetCheckerHandler{
   InternetCheckerHandler._internal();
 
   Future<bool> checkConnectionInternet() async{
-    final connectivityResult = await (Connectivity().checkConnectivity());
+    final connectivityResult = await Connectivity().checkConnectivity();
     bool isConnectionInternet = true;
     if(connectivityResult.isEmpty) {
       return false;
     }
-    if (connectivityResult.first == ConnectivityResult.mobile) {
-      // I am connected to a mobile network.
-    } else if (connectivityResult.first == ConnectivityResult.wifi) {
-      // I am connected to a wifi network.
-    } else if (connectivityResult.first == ConnectivityResult.ethernet) {
-      // I am connected to a ethernet network.
-    } else if (connectivityResult.first == ConnectivityResult.vpn) {
-      // I am connected to a vpn network.
-      // Note for iOS and macOS:
-      // There is no separate network interface type for [vpn].
-      // It returns [other] on any device (also simulator)
-    } else if (connectivityResult.first == ConnectivityResult.bluetooth) {
-      // I am connected to a bluetooth.
-    } else if (connectivityResult.first == ConnectivityResult.other) {
-      // I am connected to a network which is not in the above mentioned networks.
-    } else if (connectivityResult.first == ConnectivityResult.none) {
-      // I am not connected to any network.
+
+    if(connectivityResult.first == ConnectivityResult.none) {
       isConnectionInternet = false;
     }
-    if(!isConnectionInternet)
-    {
-      //TODO : Implement the code to show the dialog
-    }
+    // if (connectivityResult.first == ConnectivityResult.mobile) {
+    //   // I am connected to a mobile network.
+    // } else if (connectivityResult.first == ConnectivityResult.wifi) {
+    //   // I am connected to a wifi network.
+    // } else if (connectivityResult.first == ConnectivityResult.ethernet) {
+    //   // I am connected to a ethernet network.
+    // } else if (connectivityResult.first == ConnectivityResult.vpn) {
+    //   // I am connected to a vpn network.
+    //   // Note for iOS and macOS:
+    //   // There is no separate network interface type for [vpn].
+    //   // It returns [other] on any device (also simulator)
+    // } else if (connectivityResult.first == ConnectivityResult.bluetooth) {
+    //   // I am connected to a bluetooth.
+    // } else if (connectivityResult.first == ConnectivityResult.other) {
+    //   // I am connected to a network which is not in the above mentioned networks.
+    // } else if (connectivityResult.first == ConnectivityResult.none) {
+    //   // I am not connected to any network.
+    //   isConnectionInternet = false;
+    // }
+    //TODO: implement check internet connection
     return isConnectionInternet;
   }
 }
