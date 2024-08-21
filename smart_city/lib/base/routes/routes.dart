fb893d91d77d5b0e68186ab0fe494dd95c1402e4
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-21 17:13:55 +0700

    feat: auto route when use app more than one time

--- smart_city/lib/base/routes/routes.dart
+++ smart_city/lib/base/routes/routes.dart
@@ -1,14 +1,39 @@
 import 'package:go_router/go_router.dart';
+import 'package:smart_city/base/sqlite_manager/sqlite_manager.dart';
+import 'package:smart_city/base/store/shared_preference_data.dart';
 import 'package:smart_city/view/login/forgot_password/forgot_password_ui.dart';
 import 'package:smart_city/view/splash_screen.dart';
+import 'package:smart_city/view/welcome_screen.dart';
 import 'package:smart_city/view/login/login_ui.dart';
 import 'package:smart_city/view/map/map_ui.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/',
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const SplashScreen(),
+        builder: (context, state) => const WelcomeScreen(),
+        redirect: (context, state) async{
+          final bool isFirstUsingApp = await SharedPreferenceData.getHaveFirstUsingApp();
+          if(isFirstUsingApp){
+            return '/';
+          }else{
+            return '/splash';
+          }
+        },
+      ),
+      GoRoute(
+          path: '/splash',
+          builder: (context, state) => const SplashScreen(),
+          redirect: (context, state)async{
+            final bool isAuthenticated = await SqliteManager.getInstance.getCurrentLoginUserInfo() != null;
+            //TODO: change the condition
+            if(isAuthenticated){
+              return '/login';
+            }else{
+              return '/map';
+            }
+          }
       ),
       GoRoute(
         name: 'map',
@@ -26,7 +51,7 @@ final GoRouter router = GoRouter(
                 return ForgotPassword(phoneNumber: phone??'');
               },
             )
-          ]
-      )
-]
+          ],
+      ),
+],
 );
