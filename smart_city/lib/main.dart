6548a33772a18b7e0db423d220a67816ce1f5a1c
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-24 20:16:32 +0700

    refactor: change class name

--- smart_city/lib/main.dart
+++ smart_city/lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:smart_city/base/routes/routes.dart';
 import 'package:smart_city/base/store/cached_storage.dart';
-import 'base/firebase_manager/firebase_manager.dart';
+import 'package:smart_city/controller/helper/map_helper.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
-  //await initialService();
+  await initialService();
   //FirebaseMessaging.onBackgroundMessage((_)=>FirebaseManager.getInstance.firebaseMessagingBackgroundHandler(_));
-  runApp(const MyApp());
+  runApp(const App());
 }
 
 Future<void> initialService()async {
   await SharedPreferencesStorage().initSharedPreferences();
-  FirebaseManager.getInstance.initialFirebase();
+  await MapHelper.getInstance().getPermission();
+  //FirebaseManager.getInstance.initialFirebase();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,4 +27,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
