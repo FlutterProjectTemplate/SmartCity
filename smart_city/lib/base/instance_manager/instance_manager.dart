4458ee5a09df5bc0047bdde51784bc0adadb03dc
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-26 14:54:57 +0700

    feat: create error login message

--- smart_city/lib/base/instance_manager/instance_manager.dart
+++ smart_city/lib/base/instance_manager/instance_manager.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 class InstanceManager{
   static final InstanceManager _singletonBlocManager = InstanceManager._internal();
   static InstanceManager get getInstance => _singletonBlocManager;
@@ -6,6 +5,9 @@ class InstanceManager{
     return _singletonBlocManager;
   }
   InstanceManager._internal();
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  BuildContext ?rootContext;
+  String _errorLoginMessage = 'Authentication Failure';
+  String get errorLoginMessage => _errorLoginMessage;
+  void setErrorLoginMessage(String message){
+    _errorLoginMessage = message;
+  }
 }
