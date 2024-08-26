4cdd10978ecefdcd0e8af4cf353e33cc18dad7c9
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-26 14:50:17 +0700

    feat: create profile screen

--- smart_city/lib/view/setting/setting_ui.dart
+++ smart_city/lib/view/setting/setting_ui.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:smart_city/base/sqlite_manager/sqlite_manager.dart';
 import 'package:smart_city/base/store/shared_preference_data.dart';
+import 'package:smart_city/base/widgets/button.dart';
+import 'package:smart_city/base/widgets/custom_alert_dialog.dart';
 import 'package:smart_city/constant_value/const_colors.dart';
 import 'package:smart_city/constant_value/const_fonts.dart';
+import 'package:smart_city/model/user/user_info.dart';
 
 class SettingUi extends StatefulWidget {
   const SettingUi({super.key});
@@ -39,92 +42,101 @@ class _SettingUiState extends State<SettingUi> {
         ),
       ),
       backgroundColor: ConstColors.surfaceColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
-            child: Text("General",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
-          ),
-          Row(
-            children: [
-              const SizedBox(width: 30),
-              const Icon(Icons.language,color: ConstColors.secondaryColor,size: 30,),
-              const SizedBox(width: 15),
-              Text('Language',style: ConstFonts().copyWithTitle(fontSize: 16),),
-              SizedBox(width: width/2.8),
-              DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  dropdownColor: ConstColors.surfaceColor,
-                  value: _selectedLanguage,
-                  items: _languages.map((String language) {
-                    return DropdownMenuItem<String>(
-                      value: language,
-                      child: Text(language,style: ConstFonts().copyWithTitle(fontSize: 17)),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedLanguage = newValue!;
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              const SizedBox(width: 30),
-              const Icon(Icons.fingerprint_outlined,color: ConstColors.secondaryColor,size: 30,),
-              const SizedBox(width: 15),
-              Text('Sign in with fingerprint',style: ConstFonts().copyWithTitle(fontSize: 16),),
-              SizedBox(width: width/5.4),
-              Switch(
-                value: _isFingerprintEnabled,
-                activeTrackColor: ConstColors.primaryColor,
-                activeColor: Colors.white,
-                inactiveThumbColor: Colors.white,
-                inactiveTrackColor: ConstColors.tertiaryColor,
-                onChanged: (bool newValue)async{
-                  if (newValue) {
-                    bool authenticated = await SqliteManager.getInstance.authenticate();
-                    if (authenticated) {
-                      await SharedPreferenceData.turnOnSignInBiometric();
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
+              child: Text("General",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
+            ),
+            Row(
+              children: [
+                const SizedBox(width: 30),
+                const Icon(Icons.language,color: ConstColors.secondaryColor,size: 30,),
+                const SizedBox(width: 15),
+                Text('Language',style: ConstFonts().copyWithTitle(fontSize: 16),),
+                SizedBox(width: width/2.8),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    dropdownColor: ConstColors.surfaceColor,
+                    value: _selectedLanguage,
+                    items: _languages.map((String language) {
+                      return DropdownMenuItem<String>(
+                        value: language,
+                        child: Text(language,style: ConstFonts().copyWithTitle(fontSize: 17)),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
                       setState(() {
-                        _isFingerprintEnabled = true;
+                        _selectedLanguage = newValue!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const SizedBox(width: 30),
+                const Icon(Icons.fingerprint_outlined,color: ConstColors.secondaryColor,size: 30,),
+                const SizedBox(width: 15),
+                Text('Sign in with fingerprint',style: ConstFonts().copyWithTitle(fontSize: 16),),
+                SizedBox(width: width/5.4),
+                Switch(
+                  value: _isFingerprintEnabled,
+                  activeTrackColor: ConstColors.primaryColor,
+                  activeColor: Colors.white,
+                  inactiveThumbColor: Colors.white,
+                  inactiveTrackColor: ConstColors.tertiaryColor,
+                  onChanged: (bool newValue)async{
+                    if (newValue) {
+                      bool authenticated = await SqliteManager.getInstance.authenticate();
+                      if (authenticated) {
+                        await SharedPreferenceData.turnOnSignInBiometric();
+                        setState(() {
+                          _isFingerprintEnabled = true;
+                        });
+                      }
+                    } else {
+                      setState(() {
+                        _isFingerprintEnabled = false;
                       });
                     }
-                  } else {
-                    setState(() {
-                      _isFingerprintEnabled = false;
-                    });
-                  }
-                },
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          _lineButton(title: "Log out", icon: Icons.logout_rounded,
-              onPressed: ()async{
-                context.go('/login');
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            _lineButton(title: "Add widget",icon:  Icons.widgets_rounded,onPressed: (){}),
+            Padding(
+              padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
+              child: Text("Account",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
+            ),
+            _lineButton(title: "Your profile",icon:  Icons.person, onPressed: ()async{
+              UserInfo? userInfo = await SqliteManager.getInstance.getCurrentLoginUserInfo();
+              context.go('/map/setting/profile',extra: userInfo);
+            }),
+            _lineButton(title: "Change your password",icon:  Icons.password_rounded,onPressed:  (){}),
+            Padding(
+              padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
+              child: Text("Support us",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
+            ),
+            _lineButton(title: "Feedback",icon:  Icons.mail_rounded,onPressed: (){}),
+            _lineButton(title: "Rate this app", icon: Icons.star_rate_rounded, onPressed: (){}),
+            _lineButton(title: "Privacy policy", icon: Icons.privacy_tip_rounded,onPressed: (){}),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: ()async{
                 await SharedPreferenceData.setLogOut();
-          }),
-          Padding(
-            padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
-            child: Text("Account",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
-          ),
-          _lineButton(title: "Your profile",icon:  Icons.person, onPressed: (){}),
-          _lineButton(title: "Change your password",icon:  Icons.password_rounded,onPressed:  (){}),
-          Padding(
-            padding: const EdgeInsets.only(left: 15,top: 5,bottom: 15),
-            child: Text("Term of service",style: ConstFonts().copyWithTitle(fontSize: 20,color: ConstColors.tertiaryColor),),
-          ),
-          _lineButton(title: "Support",icon:  Icons.support_agent_rounded,onPressed: (){}),
-          _lineButton(title: "Copyright", icon: Icons.copyright_rounded,onPressed: (){}),
-          _lineButton(title: "Version",icon:  Icons.new_releases_rounded,onPressed:  (){}),
-          _lineButton(title: "Rate this app", icon: Icons.star_rate_rounded, onPressed: (){})
-        ],
+                context.go('/login');
+              },
+              child: Center(
+                  child: Button(width: width-50, height: 50, color: ConstColors.primaryColor, isCircle: false, child: Text("Log out",style:ConstFonts().copyWithTitle(fontSize: 18))).getButton()),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -133,7 +145,6 @@ class _SettingUiState extends State<SettingUi> {
     _isFingerprintEnabled = await SharedPreferenceData.checkSignInBiometric();
     setState(() {});
   }
-
 
   Widget _lineButton({required String title,required IconData icon,required Function() onPressed}){
     return Padding(
