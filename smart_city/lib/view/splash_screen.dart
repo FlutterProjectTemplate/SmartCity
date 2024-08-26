ba669f2a54fb113e21929f311c8dcdef573640ae
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-26 14:49:17 +0700

    refactor: decrease animation time

--- smart_city/lib/view/splash_screen.dart
+++ smart_city/lib/view/splash_screen.dart
@@ -3,12 +3,13 @@ import 'package:go_router/go_router.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:smart_city/base/store/shared_preference_data.dart';
 import 'package:smart_city/constant_value/const_colors.dart';
+import 'package:smart_city/controller/helper/map_helper.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(milliseconds: 3000), () {
+    Future.delayed(const Duration(milliseconds: 1800), () async{
       SharedPreferenceData.isLogIn().then((isLogIn){
         if(isLogIn) {
           GoRouter.of(context).go('/map');
