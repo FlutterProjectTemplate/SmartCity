b8b0af973c1442e67e81fdd53f7c0399f1c025d0
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-26 17:03:18 +0700

    refactor: add new path for profile and forgot password

--- smart_city/lib/base/routes/routes.dart
+++ smart_city/lib/base/routes/routes.dart
@@ -1,7 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:smart_city/base/store/shared_preference_data.dart';
+import 'package:smart_city/model/user/user_info.dart';
 import 'package:smart_city/view/login/forgot_password/forgot_password_ui.dart';
 import 'package:smart_city/view/login/login_ui_welcome_back.dart';
+import 'package:smart_city/view/setting/profile_screen.dart';
 import 'package:smart_city/view/setting/setting_ui.dart';
 import 'package:smart_city/view/splash_screen.dart';
 import 'package:smart_city/view/welcome_screen.dart';
@@ -10,55 +12,65 @@ import 'package:smart_city/view/map/map_ui.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const WelcomeScreen(),
-        redirect: (context, state) async{
-          final bool isFirstUsingApp = await SharedPreferenceData.getHaveFirstUsingApp();
-          if(isFirstUsingApp){
-            return '/';
-          }else{
-            return '/splash';
-          }
-        },
-      ),
-      GoRoute(
-          path: '/splash',
-          builder: (context, state) => const SplashScreen(),
-      ),
-      GoRoute(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const WelcomeScreen(),
+      redirect: (context, state) async{
+        final bool isFirstUsingApp = await SharedPreferenceData.getHaveFirstUsingApp();
+        if(isFirstUsingApp){
+          return '/';
+        }else{
+          return '/splash';
+        }
+      },
+    ),
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) =>  const SplashScreen(),
+    ),
+    GoRoute(
         name: 'map',
         path: '/map',
         builder: (context, state) => const MapUi(),
         routes: [
-          GoRoute(path: 'setting', builder: (context, state) => const SettingUi())
+          GoRoute(
+              path: 'setting',
+              builder: (context, state) => const SettingUi(),
+              routes: [
+                GoRoute(
+                  path: 'profile',
+                  builder: (context, state) {
+                    final userInfo = state.extra as UserInfo?;
+                    return ProfileScreen(userInfo: userInfo);
+                  },
+                )
+              ]
+          ),
         ]
-      ),
-      GoRoute(
-          path: '/welcomeBackSignIn',
-          builder: (context, state) => LoginUiWelcomeBack(),
-      ),
-      GoRoute(
-          path: '/login',
-          builder: (context, state) => LoginUi(),
-          routes: [
-            GoRoute(
-              path: 'forgot-password/:phone',
-              builder: (context, state) {
-                final phone = state.pathParameters['phone'];
-                return ForgotPassword(phoneNumber: phone??'');
-              },
-            )
-          ],
-        redirect: (context, state) async{
-          bool isSignedInOneTime = await SharedPreferenceData.isCheckUserSignedIn();
-          if(isSignedInOneTime){
-            return '/welcomeBackSignIn';
-          }else{
-            return '/login';
-          }
-        },
-      ),
-],
+    ),
+    GoRoute(
+      path: '/welcomeBackSignIn',
+      builder: (context, state) => LoginUiWelcomeBack(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginUi(),
+      redirect: (context, state) async{
+        bool isSignedInOneTime = await SharedPreferenceData.isCheckUserSignedIn();
+        if(isSignedInOneTime){
+          return '/welcomeBackSignIn';
+        }else{
+          return '/login';
+        }
+      },
+    ),
+    GoRoute(
+      path: '/forgot-password/:phone',
+      builder: (context, state) {
+        final phone = state.pathParameters['phone'];
+        return ForgotPassword(phoneNumber: phone??'');
+      },
+    )
+  ],
 );
