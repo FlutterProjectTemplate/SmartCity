7e52746a7d81b595bfa2983df2ac9586e78c20f3
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-26 17:01:16 +0700

    feat: add SnackBar for display information

--- smart_city/lib/base/instance_manager/instance_manager.dart
+++ smart_city/lib/base/instance_manager/instance_manager.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:smart_city/constant_value/const_colors.dart';
+import 'package:smart_city/constant_value/const_fonts.dart';
+
 class InstanceManager{
   static final InstanceManager _singletonBlocManager = InstanceManager._internal();
   static InstanceManager get getInstance => _singletonBlocManager;
@@ -5,9 +9,19 @@ class InstanceManager{
     return _singletonBlocManager;
   }
   InstanceManager._internal();
+
   String _errorLoginMessage = 'Authentication Failure';
   String get errorLoginMessage => _errorLoginMessage;
   void setErrorLoginMessage(String message){
     _errorLoginMessage = message;
+  }
+
+  void showSnackBar({required BuildContext context,required String text}){
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+          SnackBar(content: Text(text,style: ConstFonts().copyWithTitle(fontSize: 16),),
+            backgroundColor: ConstColors.surfaceColor,
+          ));
   }
 }
