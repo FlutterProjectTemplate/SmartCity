fcd59c84341c637f384c14b1d142372d7f8855b8
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-28 09:02:47 +0700

    refactor: add const size resize for screen

--- smart_city/lib/constant_value/const_size.dart
+++ smart_city/lib/constant_value/const_size.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/material.dart';
-class ConstSize{
-    static double getHeight(BuildContext context){
-      return MediaQuery.of(context).size.height;
-    }
+import 'package:smart_city/base/common/responsive_info.dart';
+import 'package:smart_city/base/resizer/fetch_pixel.dart';
 
-    static double getWidth(BuildContext context){
-      return MediaQuery.of(context).size.width;
-    }
+class Dimens{
+  static double size15Horizontal = FetchPixel.getPixelWidth(15.0);
 
+  static double size20Horizontal = FetchPixel.getPixelWidth(20.0);
+
+  static double size10Vertical = FetchPixel.getPixelHeight(10.0);
+
+  static double size15Vertical = FetchPixel.getPixelHeight(15.0);
+
+  static double get size20Vertical {return FetchPixel.getPixelHeight(!ResponsiveInfo.isTablet()?20.0:22.0);}
+
+  static double get size50Vertical {return FetchPixel.getPixelHeight(!ResponsiveInfo.isTablet()?50.0:55.0);}
 }
