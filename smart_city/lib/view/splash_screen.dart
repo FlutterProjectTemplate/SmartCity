03128c5502180c5288d0762c14b387caddd4d9ea
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-28 09:08:56 +0700

    fix: fix bugs access widget's ancestor not safe

--- smart_city/lib/view/splash_screen.dart
+++ smart_city/lib/view/splash_screen.dart
@@ -3,24 +3,31 @@ import 'package:go_router/go_router.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:smart_city/base/store/shared_preference_data.dart';
 import 'package:smart_city/constant_value/const_colors.dart';
-import 'package:smart_city/controller/helper/map_helper.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
     Future.delayed(const Duration(milliseconds: 1800), () async{
       SharedPreferenceData.isLogIn().then((isLogIn)async{
         if(isLogIn) {
-          await MapHelper.getInstance().getCurrentLocation();
-          GoRouter.of(context).go('/map');
+          context.go('/map');
         } else {
-          await MapHelper.getInstance().getCurrentLocation();
-          GoRouter.of(context).go('/login');
+          context.go('/login');
         }
       });
     });
-    return  Scaffold(
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
           child: LoadingAnimationWidget.inkDrop(
               color: ConstColors.primaryColor,
