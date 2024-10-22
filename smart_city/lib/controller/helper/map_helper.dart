e1eddfb91e14aeff9448f94cdfe3f17cd22b73e2
TruongGlacio <fftresource@gmail.com>
2024-10-22 11:43:03 +0700

    update

--- smart_city/lib/controller/helper/map_helper.dart
+++ smart_city/lib/controller/helper/map_helper.dart
@@ -136,35 +136,35 @@ class MapHelper {
   }
 
   Future<bool> getPermission() async {
-    await permission_handler.Permission.locationWhenInUse.request();
-    if (await permission_handler
-        .Permission.locationWhenInUse.serviceStatus.isEnabled) {
-      LocationPermission permission;
-      bool serviceEnabled;
-
-      try {
-        serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      } on PlatformException catch (err) {
-        debugPrint(err.message);
-        serviceEnabled = false;
-      }
-
-      if (!serviceEnabled) {
-        return false;
-      }
-
-      permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.denied) {
-          return false;
-        }
-      }
-
-      return true;
-    } else {
-      return false;
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
     }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return true;
   }
 
   Future<void> listenLocation({Function(Position?)? onChangePosition, int? timeLimit, Duration? intervalDuration}) async {
@@ -223,16 +223,11 @@ class MapHelper {
 
 
   Future<void> getCurrentLocationData()async{
-    if(await getPermission()){
+      await getPermission();
       Position locationData = await Geolocator.getCurrentPosition();
       currentLocation = LatLng(locationData.latitude, locationData.longitude);
       print("get location:${currentLocation.toString()}");
       location = locationData;
-    }
-    else
-      {
-        print("get location is Denied");
-      }
   }
 
   void updateCurrentLocation(Position newLocation) {
